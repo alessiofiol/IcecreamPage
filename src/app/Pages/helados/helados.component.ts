3f205db0fc352629fdca05e30c725557b1c7b339
alessiofiol <alessiofiol@gmail.com>
2022-04-19 14:59:35 +0200

    commit

--- src/app/Pages/helados/helados.component.ts
+++ src/app/Pages/helados/helados.component.ts
@@ -1,5 +1,6 @@
 import { UserServices } from 'src/app/servicios/servicios.service';
 import { Component, OnInit } from '@angular/core';
+import { SaboresService } from 'src/app/Core/services/sabores.service';
 
 @Component({
   selector: 'app-helados',
@@ -8,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   providers: [UserServices]
 })
 export class HeladosComponent implements OnInit {
+  public saboresList: any[] = [];
+  public seleccionado: string[] =[];
 
-  constructor() { }
+  constructor(public saboreservice: SaboresService) {
+    this.saboreservice.getSabores().forEach(element => {
+     
+      this.saboresList.push(element)
+      // console.log(this.saboresList)
+     
+  
+    }); 
+   }
 
   ngOnInit(): void {
   }
-
+  public saborselection:any = []; 
+  onchangesabor ($event:any) {
+    const saboradd= this.seleccionado
+    let currentSaboresList: [any] = this.saboresList[0];
+    
+    let currentSabor = currentSaboresList.find((sabor) => {
+      return sabor.image == saboradd;
+    })
+    this.saborselection.splice(0,1,currentSabor)
+  }
 }
--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -13,34 +13,17 @@ import { SaboresService } from 'src/app/Core/services/sabores.service';
 
 export class HomeComponent implements OnInit {
   
-  public saboresList: any[] = [];
-  public seleccionado: string[] =[];
-
-
-  constructor(public saboreservice: SaboresService) { 
-    this.saboreservice.getSabores().forEach(element => {
-     
-      this.saboresList.push(element)
-      // console.log(this.saboresList)
-     
   
-    }); 
+
+  constructor() { 
+   
 
   }
   ngOnInit(): void {    
 
   }
-  public saborselection:any = []; 
 
-  onchangesabor ($event:any) {
-    const saboradd= this.seleccionado
-    let currentSaboresList: [any] = this.saboresList[0];
-    
-    let currentSabor = currentSaboresList.find((sabor) => {
-      return sabor.image == saboradd;
-    })
-    this.saborselection.splice(0,1,currentSabor)
-  }
+ 
     
 
   
